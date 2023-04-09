ed8f4c0188dae28c1f7a7bcc6822ffa56375d152
Andreas Backström <mozzo242@gmail.com>
2023-04-09 14:50:26 +0200

    Add get files endpoints

--- src/api/routes/site.py
+++ src/api/routes/site.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, abort, current_app
+from flask import Blueprint, request, jsonify, abort, current_app, send_from_directory
 from flask_jwt_extended import (create_access_token, create_refresh_token,
                                 jwt_required, get_jwt_identity, get_jwt)
 from api.models import Folder, FolderSchema, User, Site, SiteSchema, File, FileSchema
@@ -37,6 +37,78 @@ def get_folders_in_site(id):
             "message": "You do not have access to this site"
         }), 401
 
+@site_endpoint.route("/v1/sites/<site_id>/folders/<folder_id>")
+@jwt_required()
+def get_folder_by_id(site_id, folder_id):
+    folders_schema = FolderSchema()
+    current_user = User.find_by_email(get_jwt_identity())
+    allowed_site = Site.query.filter(Site.id==site_id, Site.members.any(id=current_user.id)).first() is not None
+    if allowed_site:
+        folders = Folder.query.filter(Folder.id==folder_id, Folder.site_id==site_id).first()
+        if folders:
+            return jsonify(folders_schema.dump(folders))
+        else:
+            return jsonify({
+                "error": "Not found",
+                "message": "Folder not found"
+            }), 404
+    else:
+        return jsonify({
+            "error": "Unauthorized access",
+            "message": "You do not have access to this site"
+        }), 401
+
+@site_endpoint.route("/v1/sites/<site_id>/files/<file_id>")
+@site_endpoint.route("/v1/sites/<site_id>/folders/<folder_id>/files/<file_id>")
+@site_endpoint.route("/v1/sites/<site_id>/files/<file_id>.<ext>")
+@site_endpoint.route("/v1/sites/<site_id>/folders/<folder_id>/files/<file_id>.<ext>")
+@jwt_required()
+def get_file(site_id, file_id, folder_id=None, ext=None):
+    file_schema = FileSchema()
+    current_user = User.find_by_email(get_jwt_identity())
+    allowed_site = Site.query.filter(Site.id==site_id, Site.members.any(id=current_user.id)).first() is not None
+    if allowed_site:
+        files = File.query.filter(File.id==file_id, File.site_id==site_id, File.folder_id==folder_id).first()
+        if files:
+            if ext:
+                print("EXT FOUND!")
+                print(ext)
+                print(os.path.join(current_app.config["DATA_FOLDER"], site_id))
+                print(str(files.id + files.ext))
+                return send_from_directory(os.path.join(os.getcwd(), current_app.config["DATA_FOLDER"], site_id), str(files.id + files.ext))
+            else:
+                return jsonify(file_schema.dump(files))
+        else:
+            return jsonify({
+                "error": "Not found",
+                "message": "File not found"
+            }), 404
+    else:
+        return jsonify({
+            "error": "Unauthorized access",
+            "message": "You do not have access to this site"
+        }), 401
+
+@site_endpoint.route("/v1/sites/<site_id>/files")
+@jwt_required()
+def get_files_in_site(site_id):
+    file_schema = FileSchema(many=True)
+    current_user = User.find_by_email(get_jwt_identity())
+    allowed_site = Site.query.filter(Site.id==site_id, Site.members.any(id=current_user.id)).first() is not None
+    if allowed_site:
+        files = File.query.filter(File.site_id==site_id, File.folder_id==None).all()
+        if files:
+            return jsonify(file_schema.dump(files))
+        else:
+            return jsonify({
+                "error": "Not found",
+                "message": "No files in site"
+            }), 404
+    else:
+        return jsonify({
+            "error": "Unauthorized access",
+            "message": "You do not have access to this site"
+        }), 401
 
 @site_endpoint.route("/v1/sites", methods=["POST"])
 @jwt_required()
