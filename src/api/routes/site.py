9a91c258e11aad614de5bafc6749ce731dd42ced
Andreas Backström <mozzo242@gmail.com>
2023-04-09 15:03:34 +0200

    Verify extension on file is the same

--- src/api/routes/site.py
+++ src/api/routes/site.py
@@ -84,7 +84,7 @@ def get_file(site_id, file_id, folder_id=None, ext=None):
     if allowed_site:
         files = File.query.filter(File.id==file_id, File.site_id==site_id, File.folder_id==folder_id).first()
         if files:
-            if ext:
+            if ext == files.ext.replace(".", ""):
                 print("EXT FOUND!")
                 print(ext)
                 print(os.path.join(current_app.config["DATA_FOLDER"], site_id))
